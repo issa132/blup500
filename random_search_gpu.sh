60a0270a992e448fd2290fa7d74f482690cb4e6b
issa132 <issa132@hotmail.com>
2024-09-04 16:50:56 -0400

    Update random_search_gpu.sh

--- random_search_gpu.sh
+++ random_search_gpu.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --account=rrg-mechefsk
+#SBATCH --account=def-banire
 #SBATCH --gres=gpu:1        # request GPU "generic resource"
 #SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=12G           # memory per node
 #SBATCH --time=0-00:10      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
-#SBATCH --mail-user=your_username@queensu.ca   # Email to which notifications will be $
+#SBATCH --mail-user=issa132@hotmail.com   # Email to which notifications will be $
 
 
 module load python/3.6
