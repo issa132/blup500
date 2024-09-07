503d9fb77ec91ee2f6204f469c5b8740ed3eb236
issa132 <issa132@hotmail.com>
2024-09-07 15:51:33 -0400

    Update random_search_gpu.sh

--- random_search_gpu.sh
+++ random_search_gpu.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --account=def-banire
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=p100:4
-#SBATCH --ntasks-per-node=24
-#SBATCH --mem=125G
+#SBATCH --gpus-per-node=p100:2
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=127000M
 #SBATCH --time=7-00:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=issa132@hotmail.com   # Email to which notifications will be $
+
 
 nvidia-smi
 module load python/3.6
