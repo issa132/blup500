e81e0d440a0886208bbf4baf7b4232294cd052d6
issa132 <issa132@hotmail.com>
2024-09-07 14:51:00 -0400

    Update random_search_gpu.sh

--- random_search_gpu.sh
+++ random_search_gpu.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #SBATCH --account=def-banire
-#SBATCH --gres=gpu:1        # request GPU "generic resource"
-#SBATCH --cpus-per-task=16   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=12G           # memory per node
+#SBATCH --nodes=1
+#SBATCH --gpus-per-node=p100:4
+#SBATCH --ntasks-per-node=24
+#SBATCH --exclusive
+#SBATCH --mem=125G
 #SBATCH --time=7-00:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=issa132@hotmail.com   # Email to which notifications will be $
 
-
+nvidia-smi
 module load python/3.6
 source ~/jupyter1/bin/activate
 
-python train_model_tcn.py
+python WrightFisher.py
